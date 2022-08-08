4f8251924b90db45cd2e58cbea51a4731aa4831c
Rafael Bortolozo <62819159+RafaelBortolozo@users.noreply.github.com>
2022-08-08 16:20:33 -0300

    (08/08) introdução

--- Blog/Models/Tag.cs
+++ Blog/Models/Tag.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Blog.Models{
 
     // CREATE TABLE [Tag] (
@@ -5,7 +8,9 @@ namespace Blog.Models{
     // [Name] VARCHAR(80) NOT NULL,
     // [Slug] VARCHAR(80) NOT NULL,
 
+    [Table("Tag")]
     public class Tag {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Slug { get; set; }
--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Linq;
+using Blog.Data;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog
 {
@@ -6,7 +10,51 @@ namespace Blog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using(var context = new BlogDataContext()){
+                // ADICIONAR
+
+                // var tag1 = new Tag {Name="ASP.NET", Slug="aspnet"};
+                // context.Tags.Add(tag1);
+                // context.SaveChanges();
+
+                // var tag2 = new Tag {Name=".NET", Slug="dotnet"};
+                // context.Tags.Add(tag2);
+                // context.SaveChanges();
+                
+                /*********************************************************/
+                // ATUALIZAR
+
+                // var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
+                // tag.Name = ".NET";
+                // tag.Slug = "dotnet";
+
+                // context.Update(tag);
+                // context.SaveChanges();
+
+                /*********************************************************/
+                // REMOVER
+                // var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
+
+                // context.Remove(tag);
+                // context.SaveChanges();
+
+                /*********************************************************/
+                // var tags = context
+                //     .Tags
+                //     .Where(x => x.Name.Contains(".NET"))
+                //     .ToList();
+
+                // foreach(var tag in tags){
+                //     Console.WriteLine(tag.Name);
+                // }
+
+                var tag = context
+                    .Tags
+                    .AsNoTracking()
+                    .FirstOrDefault(x => x.Id == 3);
+
+                Console.WriteLine(tag?.Name);
+            }
         }
     }
 }
