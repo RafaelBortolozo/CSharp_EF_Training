bc85a881917b4534facf6c5e95e603de66efda49
Rafael Bortolozo <62819159+RafaelBortolozo@users.noreply.github.com>
2022-08-08 17:49:59 -0300

    (08/08) consullta com innerjoin

--- Blog/Data/BlogDataContext.cs
+++ Blog/Data/BlogDataContext.cs
@@ -1,3 +1,4 @@
+using System;
 using Blog.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,7 +12,9 @@ namespace Blog.Data {
         public DbSet<User> Users { get; set; }
         //public DbSet<UserRole> UserRoles { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder options) 
-            => options.UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$");
+        protected override void OnConfiguring(DbContextOptionsBuilder options){
+            options.UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$");
+            options.LogTo(Console.WriteLine);
+        }
     }
 }
--- Blog/Program.cs
+++ Blog/Program.cs
@@ -12,33 +12,46 @@ namespace Blog
         {
             using var context = new BlogDataContext();
 
-            var user = new User{
-                Name = "Rafael Bortolozo",
-                Slug = "rafaelbortolozo",
-                Email = "rafaelbortolozo@outlook.com",
-                Bio = "joão ninguem",
-                Image = "https://bortobe.com",
-                PasswordHash = "senhaforte123"
-            };
+            // var user = new User{
+            //     Name = "Rafael Bortolozo",
+            //     Slug = "rafaelbortolozo",
+            //     Email = "rafaelbortolozo@outlook.com",
+            //     Bio = "joão ninguem",
+            //     Image = "https://bortobe.com",
+            //     PasswordHash = "senhaforte123"
+            // };
 
-            var category = new Category{
-                Name = "Backend",
-                Slug = "backend",
-            };
+            // var category = new Category{
+            //     Name = "Backend",
+            //     Slug = "backend",
+            // };
 
-            var post = new Post{
-                Author = user,
-                Category = category,
-                Body = "<p>Hello world</p>",
-                Slug = "Comecando-com-ef-core",
-                Summary = "Neste artigo vamos aprender EF Core",
-                Title = "Começando com Ef Core",
-                CreateDate = DateTime.Now,
-                LastUpdateDate = DateTime.Now
-            };
+            // var post = new Post{
+            //     Author = user,
+            //     Category = category,
+            //     Body = "<p>Hello world</p>",
+            //     Slug = "Comecando-com-ef-core",
+            //     Summary = "Neste artigo vamos aprender EF Core",
+            //     Title = "Começando com Ef Core",
+            //     CreateDate = DateTime.Now,
+            //     LastUpdateDate = DateTime.Now
+            // };
 
-            context.Posts.Add(post);
-            context.SaveChanges();
+            // context.Posts.Add(post);
+            // context.SaveChanges();
+
+            var posts = context
+                .Posts
+                .AsNoTracking()
+                .Include(x => x.Author) //innerjoin
+                .Include(x => x.Category) //innerjoin
+                .ThenInclude(x => x.) //innerjoin do innerjoin, pode-se obter valores de tabelas filhos
+                .OrderByDescending(x => x.LastUpdateDate)
+                .ToList();
+
+            foreach(var post in posts){
+                Console.WriteLine($"{post.Title} escrito por {post.Author?.Name}");
+            }
         }
     }
 }
